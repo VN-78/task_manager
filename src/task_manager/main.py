dfaf88e25efc1fdc75158f15d1578483b77662d6
VN-78 <vishwanath.s.god@gmail.com>
2026-08-27 20:00:48 +0530

    added the v1 endpoints

--- src/task_manager/main.py
+++ src/task_manager/main.py
@@ -1,4 +1,21 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.task_manager.api import v1_router
 
+
+def create_app() -> FastAPI:
+
+    app = FastAPI(title="Task_manager", version="0.0.1", description="task manager app")
+
+    # Attach the api v1
+    app.include_router(v1_router, prefix="/api/v1")
+
+    return app
+
+
+app = create_app()
+
+
+@app.get("/")
+def health():
+    return {"Working"}
